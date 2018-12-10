ffe70f9fac6fd19f979994efd335de529716d1ad
MartinH5 <mart_h@live.dk>
2018-12-10 21:06:33 +0100

    Update Block.java

--- Solution_MM/src/main/java/dk/klmm/blockchain/datastructures/Block.java
+++ Solution_MM/src/main/java/dk/klmm/blockchain/datastructures/Block.java
@@ -12,8 +12,34 @@ import java.util.Stack;
  * @author Micha
  */
 public class Block {
-    private int nonce = 0;
-    private Stack<Transaction> transaction = new Stack<>();
-    private String previousHash = null;
-    private String thisBlocksHash = null;
+    //private int nonce = 0;
+    //private Stack<Transaction> transaction = new Stack<>();
+    //private String previousHash = null;
+    //private String thisBlocksHash = null;
+    private int previousHash; 
+    String[] transactions; 
+    
+    private int blockHash;
+
+    public Block(int previousHash, String[] transactions) {
+        this.previousHash = previousHash;
+        this.transactions = transactions;
+        
+        Object[] contents = {Arrays.hashCode(transactions), previousHash};
+        this.blockHash = Arrays.hashCode(contents);
+    }
+
+    public int getPreviousHash() {
+        return previousHash;
+    }
+
+    public String[] getTransactions() {
+        return transactions;
+    }
+
+    public int getBlockHash() {
+        return blockHash;
+    }
+    
+    
 }
